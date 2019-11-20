0efe85c5384bf89450d9fc86b818d5bc41434a7f
barros.carlosds@gmail.com <barros.carlosd@gmail.com>
2019-11-20 20:24:10 -0300

    test staticfiles

--- scrumme/settings.py
+++ scrumme/settings.py
@@ -129,13 +129,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR.child('staticfiles')
+# STATIC_ROOT = BASE_DIR.child('staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR.child("static"),
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR.child("static"),
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 MEDIA_URL = "/media/"
