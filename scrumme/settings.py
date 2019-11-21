e8cffb45aa3a8e8979781a5830b6beca5824a380
barros.carlosds@gmail.com <barros.carlosd@gmail.com>
2019-11-21 01:44:24 -0300

    add whitenoise

--- scrumme/settings.py
+++ scrumme/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'whitenoise.runserver_nostatic',
+
     'crispy_forms',
     'tempus_dominus',
 
@@ -59,6 +61,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'scrumme.urls'
