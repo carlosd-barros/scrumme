d15474b7a877851800f37de1d63210c225f6e045
carlos.ds <cdsb.to@gmail.com>
2019-11-21 13:49:16 -0300

    add BaseFormHelper

--- core/forms/update.py
+++ core/forms/update.py
@@ -16,12 +16,24 @@ from tempus_dominus.widgets import DatePicker, TimePicker, DateTimePicker
 from django.contrib.auth.models import User
 from core.models import Jogador, Classe, Equipe, Quest
 
-from .base import ModelBaseFormHelper, ModelDatePickerForm
+from .base import ModelBaseFormHelper, ModelDatePickerForm, BaseFormHelper
 
 logger = logging.getLogger(__name__)
 
 
 class JogadorUpdateForm(ModelBaseFormHelper):
+    password1 = forms.CharField(
+        label='Senha',
+        widget= forms.PasswordInput,
+        max_length=20,
+        required=False
+    )
+    password2 = forms.CharField(
+        label='Confirme sua senha',
+        widget= forms.PasswordInput,
+        max_length=20,
+        required=False
+    )
     avatar = forms.FileField(
         label='Avatar',
         required=False
@@ -32,12 +44,28 @@ class JogadorUpdateForm(ModelBaseFormHelper):
         fields = [
             'username', 'email',
             'first_name', 'last_name',
+            'password1', 'password2',
             'avatar'
         ]
 
     def __init__(self, *args, **kwargs):
-        instance = kwargs.get('instance')
+        instance = kwargs.get('instance', None)
         super(JogadorUpdateForm, self).__init__(*args, **kwargs)
+        self.helper.layout = Layout(
+            'username',
+            Row(
+                Column('first_name', css_class='form-group col-md-6 mb-0'),
+                Column('last_name', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
+            ),
+            'email',
+            Row(
+                Column('password1', css_class='form-group col-md-6 mb-0'),
+                Column('password2', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
+            ),
+            'avatar',
+        )
 
         if instance:
             self.fields['avatar'].initial = instance.jogador.avatar
@@ -49,7 +77,7 @@ class JogadorUpdateForm(ModelBaseFormHelper):
             file_extension = avatar.name.split('.')[-1]
 
             try:
-                logger.debug('verificando extensão do arquivo')
+                print('verificando extensão do arquivo')
                 if file_extension in settings.ALLOWED_UPLOAD_FILETYPES:
                     logger.debug('verificando tamanho do arquivo')
                     if avatar.size > settings.ALLOWED_UPLOAD_MAXSIZE:
