22e6d7c53351aa97d31b6b8ce5aba9cf23f43613
carlosd-barros <barros.carlosd@gmail.com>
2019-11-24 04:48:44 -0300

    remove print

--- scrumme/settings.py
+++ scrumme/settings.py
@@ -30,7 +30,6 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
-print(f"aquiiiiiiiiii: {DEBUG}")
 TEMPLATE_DEBUG = DEBUG
 ALLOWED_HOSTS = ['*']
 
